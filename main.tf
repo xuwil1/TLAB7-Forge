b06c4396e91ce08f0508c3f2dee4251e92aa6ff9
wxu <xuwillie1@gmail.com>
2026-07-30 21:35:25 -0400

    Changed cidr_block

--- main.tf
+++ main.tf
@@ -29,10 +29,10 @@ resource "aws_security_group" "sabotaged_sg" {
   name        = "tlab7-exposed-sg"  
   description = "A dangerously exposed security group"  
   
-  ingress {  
-    from_port   = 22  
-    to_port     = 22  
-    protocol    = "tcp"  
-    cidr_blocks = ["100.12.79.25/32"] # SABOTAGE: SSH exposed to the world  
-  }  
+  ingress {
+  from_port   = 22
+  to_port     = 22
+  protocol    = "tcp"
+  cidr_blocks = ["100.12.79.25/32"] #tfsec:ignore:aws-vpc-no-public-ingress-sgr
+}  
 }
